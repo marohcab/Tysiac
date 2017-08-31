73855f25c5f20e2a764bebc97ab3d55fe1f7ea2c
DoneQ <marcinrubach@gmail.com>
2017-08-31 16:45:15 +0200

    buttony

--- app/src/main/java/com/example/bachor/tysiac/Two_Players.java
+++ app/src/main/java/com/example/bachor/tysiac/Two_Players.java
@@ -8,8 +8,8 @@ import android.widget.TextView;
 
 public class Two_Players extends AppCompatActivity {
 
-    Button bomb_1;
-    Button bomb_2;
+    Button btnBomb_1;
+    Button btnBomb_2;
     TextView points_1,points_2,txt_bomb_1,txt_bomb_2;
     String test_string = "test";
     int test_int = 1;
@@ -26,14 +26,14 @@ public class Two_Players extends AppCompatActivity {
         TextView txt_bomb_2 = (TextView) findViewById(R.id.text_Bomb_2);
         points1.setText(Integer.toString(test_int));
         points2.setText(Integer.toString(test_int));
-        bomb_1 = (Button) findViewById(R.id.button_Bomb_1);
-        bomb_2 = (Button) findViewById(R.id.button_Bomb_2);
+        btnBomb_1 = (Button) findViewById(R.id.button_Bomb_1);
+        btnBomb_2 = (Button) findViewById(R.id.button_Bomb_2);
         Bomb_1();
         Bomb_2();
     }
 
     private void Bomb_1() {
-        bomb_1.setOnClickListener(
+        btnBomb_1.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
@@ -46,7 +46,7 @@ public class Two_Players extends AppCompatActivity {
         );
     }
     private void Bomb_2() {
-        bomb_2.setOnClickListener(
+        btnBomb_2.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
