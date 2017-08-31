efbda4880306aa723f090866dc18eb7fd17557a2
marohcab <los.amarenos@gmail.com>
2017-08-31 14:39:59 +0200

    Mateusz Bachor

--- app/src/main/java/com/example/bachor/tysiac/NewGame.java
+++ app/src/main/java/com/example/bachor/tysiac/NewGame.java
@@ -62,7 +62,7 @@ public class NewGame extends AppCompatActivity {
             case 1:
             {showMessage("Błąd", "Minimalna ilość graczy to: 2");}
             case 2:
-            {}
+            {{showMessage("Błąd", "Minimalna ilość graczy to: 2");}}
             case 3:
             {}
             case 4:
