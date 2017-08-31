472080d2e63ed7878c7f7be3f299828ecaa20d18
marohcab <los.amarenos@gmail.com>
2017-08-31 14:23:46 +0200

    Mateusz Bachor

--- app/src/main/java/com/example/bachor/tysiac/MainActivity.java
+++ app/src/main/java/com/example/bachor/tysiac/MainActivity.java
@@ -2,12 +2,58 @@ package com.example.bachor.tysiac;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnNew;
+    Button btnContinue;
+    Button btnLoad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnNew = (Button)findViewById(R.id.button_NewGame);
+        btnContinue = (Button)findViewById(R.id.button_ContinueGame);
+        btnLoad = (Button)findViewById(R.id.button_LoadGame);
+
+        New();
+        Continue();
+        Load();
+    }
+
+    private void New(){
+        btnNew.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                }
+        );
+    }
+
+    private void Continue(){
+        btnContinue.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                }
+        );
+    }
+
+    private void Load(){
+        btnLoad.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                    }
+                }
+        );
     }
 }
---
+++ app/src/main/java/com/example/bachor/tysiac/NewGame.java
@@ -0,0 +1,78 @@
+package com.example.bachor.tysiac;
+
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class NewGame extends AppCompatActivity {
+
+    Button btnStart, btnBack;
+
+    EditText nazwaG1, nazwaG2, nazwaG3, nazwaG4;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_game);
+        nazwaG1 = (EditText)findViewById(R.id.edit_NazwaGracz1);
+        nazwaG2 = (EditText)findViewById(R.id.edit_NazwaGracz2);
+        nazwaG3 = (EditText)findViewById(R.id.edit_NazwaGracz3);
+        nazwaG4 = (EditText)findViewById(R.id.edit_NazwaGracz4);
+        btnStart = (Button)findViewById(R.id.button_Start);
+        btnBack = (Button)findViewById(R.id.button_Back);
+
+    }
+
+    private int iluGraczy() {
+        int wynik=0;
+        if(nazwaG1.getText().toString()!="")
+            wynik++;
+        if(nazwaG2.getText().toString()!="")
+            wynik++;
+        if(nazwaG3.getText().toString()!="")
+            wynik++;
+        if(nazwaG4.getText().toString()!="")
+            wynik++;
+        return wynik;
+
+    }
+
+    private void pressStart(){
+        btnStart.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Start(iluGraczy());
+                    }
+                }
+        );
+    }
+
+    private void Start(int gracze) {
+        switch (gracze){
+            case 1:
+            {showMessage("Błąd", "Minimalna ilość graczy to: 2");}
+            case 2:
+            {}
+            case 3:
+            {}
+            case 4:
+            {}
+            default:
+            {}
+
+        }
+    }
+
+    public void showMessage(String title, String message){
+        AlertDialog.Builder builder  = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
+    }
+}
