ece37a302e78a7d7973ef0c4c3b5b4d49ef33632
marohcab <los.amarenos@gmail.com>
2017-09-01 08:37:37 +0200

    Mateusz Bachor

---
+++ app/src/main/java/com/example/bachor/tysiac/DataBase.java
@@ -0,0 +1,48 @@
+package com.example.bachor.tysiac;
+
+/**
+ * Created by Bachor on 2017-09-01.
+ */
+
+public class DataBase {
+    public static final String DATABASE_NAME = "Data.db";
+
+    /*
+
+        WSZYSTKIE DANE W GRZE
+
+     */
+
+    public static final String TABLE_NAME = "GameData";
+    public static final String ID = "ID";
+    public static final String ILOSC = "Gracze";
+    public static final String DATA = "Data";
+    public static final String RUNDA = "Runda";
+    public static final String ZACZYNA = "Zaczyna";
+    public static final String ROZDAJE = "Rozdaje";
+
+    public static final String GRACZ1_NAZWA = "Gracz1Nazwa";
+    public static final String GRACZ1_PUNKTY = "Gracz1Punkty";
+    public static final String GRACZ1_BOMBA = "Gracz1Bomba";
+
+    public static final String GRACZ2_NAZWA = "Gracz2Nazwa";
+    public static final String GRACZ2_PUNKTY = "Gracz2Punkty";
+    public static final String GRACZ2_BOMBA = "Gracz2Bomba";
+
+    public static final String GRACZ3_NAZWA = "Gracz3Nazwa";
+    public static final String GRACZ3_PUNKTY = "Gracz3Punkty";
+    public static final String GRACZ3_BOMBA = "Gracz3Bomba";
+
+    public static final String GRACZ4_NAZWA = "Gracz4Nazwa";
+    public static final String GRACZ4_PUNKTY = "Gracz4Punkty";
+    public static final String GRACZ4_BOMBA = "Gracz4Bomba";
+
+    /*
+
+        OBECNA GRA
+
+     */
+
+    public static final String TABLE_CURRENT = "Current";
+    public static final String CURRENT = "ID";
+}
